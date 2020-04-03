f2979d16b3e004ea5f16f5c39b3d1bd76b3f66af
Kaur <mkaur24@dxc.com>
2020-04-03 15:01:45 +0530

    My First Work

---
+++ GSClass/GSClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GSClass
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Array obj = new Array();
+
+            obj.FirstMaxandsecondMax2();
+
+            Console.ReadKey();
+
+
+
+
+        }
+
+    }
+
+
+
+
+}
+
+
